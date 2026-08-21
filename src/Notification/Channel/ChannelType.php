319e6d719a1a8f3fb18a1f2f1fac0161774a0313
Loïc Sapone <loic@mezcalito.fr>
2026-08-21 15:30:39 +0200

    Add Mattermost channel notifications

--- src/Notification/Channel/ChannelType.php
+++ src/Notification/Channel/ChannelType.php
@@ -5,10 +5,11 @@ declare(strict_types=1);
 namespace App\Notification\Channel;
 
 /**
- * Type de canal de notification supporté, tel que déclaré dans notifications.yaml.
+ * Supported notification channel type, as declared in notifications.yaml.
  */
 enum ChannelType: string
 {
     case Email = 'email';
     case FreeMobile = 'free_mobile';
+    case Mattermost = 'mattermost';
 }
